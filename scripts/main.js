88d4ab44f09e5ee7a9ec6fcacb70e61def7f6ff3
dorachan <doratan@gmail.com>
2016-08-29 17:29:39 +0900

    add map

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,15 @@ AfterFiver.prototype.checkSetup = function () {
 window.onload = function () {
   window.AfterFiver = new AfterFiver();
 };
+
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    scrollwheel: false,
+    zoom: 8
+  });
+}
